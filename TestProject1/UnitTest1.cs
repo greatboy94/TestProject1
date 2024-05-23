6641e986c34dd6812710ab7e135b1dd21978ef69
Buyukbek <greatboy_94@mail.ru>
2024-05-24 01:06:03 +0500

    Added docker-compose

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -2,6 +2,7 @@ using Allure.NUnit.Attributes;
 using NUnit.Allure.Core;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using WebDriverManager.DriverConfigs.Impl;
 
 namespace TestProject1;
@@ -16,6 +17,8 @@ public class Tests
     [SetUp]
     public void Setup()
     {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        driver = new RemoteWebDriver(new Uri("192.0.0.1"), chromeOptions);
         new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
         driver = new ChromeDriver();
 
